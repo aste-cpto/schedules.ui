416e01965b475441cb1778fc6b4b77a30e042a1e
Iryna Mariiko <IrynaMM497@gmail.com>
2026-08-23 15:59:45 +0300

    Fix table row numbers across paginated pages

--- src/pages/SchedulesPage/utils/RenderCell.tsx
+++ src/pages/SchedulesPage/utils/RenderCell.tsx
@@ -2,10 +2,14 @@ import type { Schedule } from '~/types/schedule'
 import type { ScheduleTableColumnKey } from '../config/scheduleTableColumns'
 // import { StatusBadge } from '../components/StatusBadge'
 
-export function renderCell(columnKey: ScheduleTableColumnKey, schedule: Schedule, index: number) {
+export function renderCell(
+  columnKey: ScheduleTableColumnKey,
+  schedule: Schedule,
+  rowNumber: number,
+) {
   switch (columnKey) {
     case 'index':
-      return index + 1
+      return rowNumber
     // case 'status':
     //   return <StatusBadge status={schedule.status} />
     case 'group':
